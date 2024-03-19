17f0a73ee2801e8573c1bda0480765347a66a08a
alban-martinant-de-preneuf <alban.martinant-de-preneuf@laplateforme.io>
2024-03-19 15:11:29 +0100

    contract is done

---
+++ contracts/HelloWorld.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+
+pragma solidity ^0.8.24;
+
+contract HelloWorld {
+    event UpdatedMessage(string oldString, string newString);
+
+    string public message;
+
+    constructor(string memory _initMessage) {
+        message =_initMessage;
+    }
+
+    function update(string memory _newMessage) external {
+        string memory oldMessage = message;
+        message = _newMessage;
+        emit UpdatedMessage(oldMessage, _newMessage);
+    }
+}
