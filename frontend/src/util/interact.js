3f9966785665b63344d0b2d3bb322afff5d948dd
alban-martinant-de-preneuf <alban.martinant-de-preneuf@laplateforme.io>
2024-03-21 09:26:33 +0100

    charging the contract

--- frontend/src/util/interact.js
+++ frontend/src/util/interact.js
@@ -1,17 +1,30 @@
-//export const helloWorldContract;
+// require('dotenv').config()
+const { ALCHEMY_API_KEY, CONTRACT_ADDRESS } = process.env;
 
-export const loadCurrentMessage = async () => { 
-  
+const alchemyKey = `wss://eth-sepolia.g.alchemy.com/v2/${ALCHEMY_API_KEY}`;
+const { createAlchemyWeb3 } = require("@alch/alchemy-web3");
+const web3 = createAlchemyWeb3(alchemyKey);
+
+const contractABI = require("../../../artifacts/contracts/HelloWorld.sol/HelloWorld.json").abi;
+const contractAddress = CONTRACT_ADDRESS;
+
+export const helloWorldContract = new web3.eth.Contract(
+    contractABI,
+    contractAddress
+);
+
+export const loadCurrentMessage = async () => {
+
 };
 
 export const connectWallet = async () => {
-  
+
 };
 
 export const getCurrentWalletConnected = async () => {
-  
+
 };
 
 export const updateMessage = async (address, message) => {
-  
+
 };
